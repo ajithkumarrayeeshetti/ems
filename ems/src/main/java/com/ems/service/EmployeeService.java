e91f8eb7102329bab3a2acb1a3b1a2c00358b98d
ajithkumarrayeeshetti <ajithkumarpatel03@gmail.com>
2026-07-13 11:04:58 +0530

    feat: implement employee service layer

--- ems/src/main/java/com/ems/service/EmployeeService.java
+++ ems/src/main/java/com/ems/service/EmployeeService.java
@@ -1,9 +1,34 @@
 package com.ems.service;
 
+import com.ems.entity.Employee;
+import com.ems.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
-    // Contains business logic
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
